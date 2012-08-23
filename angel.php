b1e1f897050fac92494df59c0d16a45bde8a3224
pandurang zambare <coepme@gmail.com>
2012-08-23 16:07:59 +0530

    headers added

--- angel.php
+++ angel.php
@@ -1,19 +1,33 @@
 <?php
 /**
- * Angellist Login
- *
- */
-	include_once("angelco.php");
-
-	$obj = new Angelco;
-	
-	if(!$obj->angelco->logged_in()){
-		$obj->angelco->login();
-		return;
-	}else {
-		echo "Thank You! You're logged in via AngelList.";
-		$userObj = $obj->angelco->call('get', 'me');
+ * CodeIgniter Angel.co API Library (http://thinkoverit.com)
+ * 
+ * Author: Pandurang Zambare, pandu@thinkoverit.com
+ * 
+ * This is Conteller Class for Codeigniter Framework.
+ **/
+class angel extends Controller 
+{
+	function angel()
+	{
+		parent::Controller();
+		session_start();
+		$this->load->library('angelco');
 	}
+	function index()
+	{
+
+		if(!$this->angelco->logged_in()){
+			$this->angelco->login();
+			return;
+		}else {
+			echo "Thank You! You're logged in via AngelList.";
+			
+			//$userObj = $this->angelco->call('get', 'me');
+			//$userFounded = $this->angelco->call('get', 'startup_roles?user_id='.$userObj->id);
+		}
+	}
+}
 
 
 ?>
